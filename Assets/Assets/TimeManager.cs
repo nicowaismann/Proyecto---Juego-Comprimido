5d8a6e0bb84db2a5d644163164cc8cc71210be1d
LuisEmbonStrizzi <101569004+LuisEmbonStrizzi@users.noreply.github.com>
2022-06-05 15:21:49 -0300

    Tiempo

---
+++ Assets/Assets/TimeManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    public Text txt_customTime;
+    float customTime;
+    bool isCounting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        customTime = 0;
+        isCounting = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.R)) 
+        {
+            isCounting = true;
+        }
+        if (isCounting) 
+        {
+            customTime += Time.deltaTime;
+        }
+        txt_customTime.text = Mathf.Floor(customTime).ToString();
+    }
+}
