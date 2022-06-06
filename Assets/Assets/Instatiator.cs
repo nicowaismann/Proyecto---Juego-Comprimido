e404d50bf45f44b3469529d39072c43940fe51f8
LuisEmbonStrizzi <101569004+LuisEmbonStrizzi@users.noreply.github.com>
2022-06-06 20:30:31 -0300

    Enemigo que genera cubos

---
+++ Assets/Assets/Instatiator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instatiator : MonoBehaviour
+{
+    public GameObject CubePrefab;
+    public GameObject CubeSpawnPoint;
+    public GameObject pelota;
+    public float ShootForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pelota.transform.position.x > 5 && pelota.transform.position.x < 5.1f)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Instantiate(CubePrefab);
+                CubePrefab.transform.position = CubeSpawnPoint.transform.position;
+                CubePrefab.GetComponent<Rigidbody>().AddForce(Vector3.forward * ShootForce, ForceMode.Impulse);
+
+            }
+        }
+    }
+   
+}
